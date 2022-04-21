0606657612cf68ba949be0b0af650a94b51d558f
carosteguig <carosteguig@gmail.com>
2022-04-21 14:40:56 -0700

    Finalized Home page and components code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.scss';
-import { BrowserRouter as }
+import './styles/partials/_resets.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation/Navgation';
+import HomePage from './pages/HomePage/HomePage';
+
+
 
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route to='/' component={HomePage} />
+      </Switch>
+
+    </Router>
+    </>
   );
 }
 
--- src/components/Navigation/Navgation.jsx
+++ src/components/Navigation/Navgation.jsx
@@ -1,13 +1,14 @@
 import './Navigation.scss';
 import { NavLink } from 'react-router-dom'; 
+import Logo from '../../assets/images/logo/logo.svg';
 
 export default function Navigation() {
 
     return (
 
-        <nav clssName='nav'>
+        <nav className='nav'>
             <NavLink to='/'>
-                <img className='nav__logo' src='' alt='Cristian Arostegui logo' />
+                <img className='nav__logo' src={Logo} alt='Cristian Arostegui logo' />
             </NavLink>
             <div className='nav__list'>
                 <NavLink className='nav__list-link' to='/' >
